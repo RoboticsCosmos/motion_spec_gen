05d869067c1faac36694df408b96d09393b39243
Vamsi Kalagaturu <vamsikalagaturu@gmail.com>
2024-12-24 16:56:25 +0100

    added hddc2b solver support for base

---
+++ gen/freddy_base_control_hddc2b.cpp
@@ -0,0 +1,439 @@
+#include <array>
+#include <string>
+#include <filesystem>
+#include <iostream>
+#include <chrono>
+#include <controllers/pid_controller.hpp>
+#include <motion_spec_utils/utils.hpp>
+#include <motion_spec_utils/math_utils.hpp>
+#include <motion_spec_utils/solver_utils.hpp>
+#include <csignal>
+
+#include <unsupported/Eigen/MatrixFunctions>
+
+#include "motion_spec_utils/log_structs.hpp"
+
+#include <hddc2b/functions/platform.h>
+#include <hddc2b/functions/solver.h>
+#include <hddc2b/functions/drive.h>
+#include <hddc2b/functions/wheel.h>
+#include <solver.h>
+
+volatile sig_atomic_t flag = 0;
+
+void handle_signal(int sig)
+{
+  static int signal_caught = 0;
+  if (!signal_caught)
+  {
+    signal_caught = 1;
+    flag = 1;
+    printf("Caught signal %d (%s)\n", sig, strsignal(sig));
+  }
+}
+
+void print_matrix(int rows, int cols, const double *a)
+{
+  printf("[");
+  for (int m_ = 0; m_ < rows; m_++)
+  {
+    printf("[");
+    for (int n_ = 0; n_ < cols; n_++)
+    {
+      printf("%10f", a[m_ + n_ * rows]);
+      if (n_ != cols - 1)
+        printf(", ");
+    }
+    printf("]");
+    if (m_ != rows - 1)
+      printf(",\n ");
+  }
+  printf("]");
+}
+
+#define NUM_DRV 4
+#define NUM_WHL_COORD 2
+#define NUM_GND_COORD 2
+#define NUM_DRV_COORD 2
+#define NUM_PLTF_COORD 3
+#define NUM_G_COORD ((NUM_DRV_COORD) * (NUM_PLTF_COORD))
+#define EPS 0.001
+
+int main(int argc, char **argv)
+{
+  // handle signals
+  struct sigaction sa;
+  sa.sa_handler = handle_signal;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+
+  for (int i = 1; i < NSIG; ++i)
+  {
+    if (sigaction(i, &sa, NULL) == -1)
+    {
+      perror("sigaction");
+    }
+  }
+
+  // read the platform force from the command line
+  double pf[3] = {0.0, 0.0, 0.0};
+  int count_limit = -1;
+  if (argc == 4)
+  {
+    pf[0] = std::stod(argv[1]);
+    pf[1] = std::stod(argv[2]);
+    pf[2] = std::stod(argv[3]);
+  }
+  else if (argc == 5)
+  {
+    pf[0] = std::stod(argv[1]);
+    pf[1] = std::stod(argv[2]);
+    pf[2] = std::stod(argv[3]);
+    count_limit = std::stoi(argv[4]);
+  }
+  else
+  {
+    printf(
+        "Usage: ./freddy_base_align <platform_force_x> <platform_force_y> <platform_torque_z>\n");
+    exit(1);
+  }
+
+  printf("count_limit: %d\n", count_limit);
+
+  // Initialize the robot structs
+  KeloBaseConfig *kelo_base_config = new KeloBaseConfig();
+  kelo_base_config->nWheels = 4;
+  int index_to_EtherCAT[4] = {3, 4, 6, 7};
+  kelo_base_config->index_to_EtherCAT = index_to_EtherCAT;
+  kelo_base_config->radius = 0.115 / 2;
+  kelo_base_config->castor_offset = 0.01;
+  kelo_base_config->half_wheel_distance = 0.0775 / 2;
+  double wheel_coordinates[8] = {0.195, 0.21, -0.195, 0.21, -0.195, -0.21, 0.195, -0.21};
+  kelo_base_config->wheel_coordinates = wheel_coordinates;
+  double pivot_angles_deviation[4] = {0.0, 0.0, 0.0, 0.0};
+  kelo_base_config->pivot_angles_deviation = pivot_angles_deviation;
+
+  // Diameter of each wheel.
+  double wheel_diameter[NUM_DRV * 2] = {
+      // [m]
+      0.115, 0.115,  // fl-r, fl-l
+      0.115, 0.115,  // rl-r, rl-l
+      0.115, 0.115,  // rr-r, rr-l
+      0.115, 0.115   // fr-r, fr-l
+  };
+
+  // Kinematic parameters of the differential drive part:
+  // Distance of the wheels from the centre between the wheels.
+  double wheel_distance[NUM_DRV] = {
+      // [m]
+      0.0775, 0.0775, 0.0775, 0.0775  // fl, rl, rr, fr
+  };
+
+  // Kinematic parameters of the castor drive part:
+  // Distance of the axle from the pivot joint's axis
+  double castor_offset[NUM_DRV] = {
+      // [m]
+      0.01, 0.01, 0.01, 0.01  // fl, rl, rr, fr
+  };
+
+  // For _singular_ platforms the relative weight between the platform-level
+  // forces, i.e. for the platform in this example it has no impact on the
+  // force distribution.
+  double w_platform[NUM_PLTF_COORD * NUM_PLTF_COORD] = {
+      // [1/N^2], [1/(N Nm)], [1/(Nm)^2]
+      1.0, 0.0, 0.0,  // xx, xy, xm
+      0.0, 1.0, 0.0,  // yx, yy, ym
+      0.0, 0.0, 1.0   // mx, my, mm
+  };
+
+  // For _redundant_ platforms the relative weight between the drive-level
+  // forces. The entries here disable the front-right drive unit.
+  double w_drive[NUM_DRV * 4] = {
+      // [1/N^2]
+      1.0, 0.0, 0.0, 1.0,  // fl-xx, fl-xy, fl-yx, fl-yy
+      1.0, 0.0, 0.0, 1.0,  // rl-xx, rl-xy, rl-yx, rl-yy
+      1.0, 0.0, 0.0, 1.0,  // rr-xx, rr-xy, rr-yx, rr-yy
+      1.0, 0.0, 0.0, 1.0   // fr-xx, fr-xy, fr-yx, fr-yy
+  };
+
+  // Weight of the angular and linear alignment distance, respectively.
+  // The weight for the front-right drive unit means that it will always have
+  // a "zero" alignment distance.
+  double w_align[NUM_DRV * 2] = {
+      //
+      1.0, 1.0,  // fl-ang, fl-lin
+      1.0, 1.0,  // rl-ang, rl-lin
+      1.0, 1.0,  // rr-ang, rr-lin
+      1.0, 1.0   // fr-ang, fr-lin
+  };
+
+  EthercatConfig *ethercat_config = new EthercatConfig();
+
+  MobileBase<Robile> freddy_base;
+  Robile robile;
+  robile.ethercat_config = ethercat_config;
+  robile.kelo_base_config = kelo_base_config;
+
+  freddy_base.mediator = &robile;
+  freddy_base.state = new MobileBaseState();
+
+  Freddy robot = {nullptr, nullptr, &freddy_base};
+
+  // get current file path
+  std::filesystem::path path = __FILE__;
+
+  // get the robot urdf path
+  std::string robot_urdf =
+      (path.parent_path().parent_path() / "urdf" / "freddy_corrected_base.urdf").string();
+
+  char ethernet_interface[100] = "eno1";
+  initialize_robot(&robot, robot_urdf, ethernet_interface);
+
+  const double desired_frequency = 1000.0;                                             // Hz
+  const auto desired_period = std::chrono::duration<double>(1.0 / desired_frequency);  // s
+  double control_loop_timestep = desired_period.count();                               // s
+  double *control_loop_dt = &control_loop_timestep;                                    // s
+
+  // pid controller variables
+  double Kp = 10.5;
+  double Ki = 0.1;
+  double Kd = 0.0;
+
+  double w_dist_prev_error[4] = {0.0, 0.0, 0.0, 0.0};
+  double w_dist_error_sum[4] = {0.0, 0.0, 0.0, 0.0};
+
+  double pf_lin_x_vel_prev_error = 0.0;
+  double pf_lin_x_vel_error_sum = 0.0;
+  double pf_lin_y_vel_prev_error = 0.0;
+  double pf_lin_y_vel_error_sum = 0.0;
+  double pf_ang_z_vel_prev_error = 0.0;
+  double pf_ang_z_vel_error_sum = 0.0;
+
+  double plat_vel_setpoint[3] = {0.1, 0.1, 0.5};
+  double plat_clip_force = 20.0;
+  double plat_sat_force = 300.0;
+
+  double pivot_vel_zero_threshold = 2.5;
+  double pivot_vel_ramp_factor = 0.1;
+  double pivot_align_error_margin = 0.25;
+
+  // Force composition matrix (from drive forces to platform force)
+  double g[NUM_DRV * NUM_G_COORD];
+
+  // Drive force that results from distributing the platform-level force
+  double f_drive[NUM_DRV * NUM_DRV_COORD];  // [N]
+  bzero(f_drive, NUM_DRV * NUM_GND_COORD * sizeof(double));
+
+  // Force at the wheel-ground contact point
+  double f_wheel[NUM_DRV * NUM_GND_COORD];  // [N]
+
+  update_base_state(robot.mobile_base->mediator->kelo_base_config,
+                    robot.mobile_base->mediator->ethercat_config);
+  get_robot_data(&robot, *control_loop_dt);
+
+  int count = 0;
+
+  bool while_condition = count_limit > 0 ? count < count_limit : true;
+
+  while (while_condition)
+  {
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    while_condition = count_limit > 0 ? count < count_limit : true;
+
+    if (flag)
+    {
+      printf("Exiting somewhat cleanly...\n");
+      free_robot_data(&robot);
+      exit(0);
+    }
+
+    printf("\ncount: %d\n", count);
+
+    get_robot_data(&robot, *control_loop_dt);
+
+    double plat_force[3] = {pf[0], pf[1], pf[2]};  // [N], [N], [Nm]
+
+    // saturation
+    for (size_t i = 0; i < 3; i++)
+    {
+      if (plat_force[i] > plat_sat_force)
+      {
+        plat_force[i] = plat_sat_force;
+      }
+      else if (plat_force[i] < -plat_sat_force)
+      {
+        plat_force[i] = -plat_sat_force;
+      }
+    }
+
+    printf("platform force: ");
+    print_array(plat_force, 3);
+
+    // printf("platform velocity: ");
+    // print_array(robot.mobile_base->state->xd_platform, 3);
+
+    printf("pivot angles:     ");
+    print_array(robot.mobile_base->state->pivot_angles, 4);
+
+    printf("pivot velocities: ");
+    print_array(robot.mobile_base->state->pivot_velocities, 4);
+
+    // Actuator torque
+    double tau_wheel[NUM_DRV * NUM_WHL_COORD];  // [Nm]
+
+    double drive_align_dsts[NUM_DRV * NUM_DRV_COORD];  // [N]
+    hddc2b_pltf_drv_algn_dst(
+        NUM_DRV, robot.mobile_base->mediator->kelo_base_config->wheel_coordinates, w_align,
+        robot.mobile_base->state->pivot_angles, plat_force, &drive_align_dsts[1], 2);
+
+    printf("\ndrive_align_dsts:\n");
+    print_matrix(NUM_DRV_COORD, NUM_DRV, drive_align_dsts);
+
+    double f_drive_ref[NUM_DRV * NUM_DRV_COORD];  // [N]
+
+    for (size_t i = 0; i < NUM_DRV; i++)
+    {
+      if (abs(drive_align_dsts[2 * i + 1]) < 40.0)
+        f_drive_ref[2 * i + 1] = 0.0;
+      else
+        f_drive_ref[2 * i + 1] = drive_align_dsts[2 * i + 1];
+
+      f_drive_ref[2 * i] = 0.0;
+    }
+
+    printf("\nf_drive_ref:\n");
+    print_matrix(NUM_DRV_COORD, NUM_DRV, f_drive_ref);
+
+    //
+    // Force distribution to wheels ...
+    //
+
+    hddc2b_pltf_frc_comp_mat(NUM_DRV,
+                             robot.mobile_base->mediator->kelo_base_config->wheel_coordinates,
+                             robot.mobile_base->state->pivot_angles, g);
+
+    double f_krnl[NUM_DRV * NUM_DRV_COORD];  // [N]
+    double f_null[NUM_DRV * NUM_DRV_COORD];  // [N]
+    hddc2b_pltf_frc_pltf_to_drv(NUM_DRV, EPS, g, w_platform, plat_force, w_drive, f_drive_ref,
+                                f_krnl, f_null);
+    printf("\nf_krnl:\n");
+    print_matrix(NUM_DRV_COORD, NUM_DRV, f_krnl);
+    printf("\nf_null:\n");
+    print_matrix(NUM_DRV_COORD, NUM_DRV, f_null);
+
+    // take y coordinate ratio of f_null to f_drive_ref
+    double f_null_ratio[NUM_DRV];
+    for (int i = 0; i < NUM_DRV; i++)
+    {
+      if (f_null[1 + i * NUM_DRV_COORD] == 0.0 || abs(f_null[1 + i * NUM_DRV_COORD]) < EPS)
+      {
+        f_null_ratio[i] = 0.0;
+        continue;
+      }
+      f_null_ratio[i] = f_drive_ref[1 + i * NUM_DRV_COORD] / f_null[1 + i * NUM_DRV_COORD];
+    }
+    printf("\nf_null_ratio:\n");
+    print_matrix(1, NUM_DRV, f_null_ratio);
+
+    // select the non-zero greatest value of f_null_ratio as the scaling factor
+    double f_scale_factor = -INFINITY;
+    for (int i = 0; i < NUM_DRV; i++)
+    {
+      if (f_null_ratio[i] == 0.0)
+        continue;
+      if (f_null_ratio[i] > f_scale_factor)
+      {
+        f_scale_factor = f_null_ratio[i];
+      }
+    }
+    // check if still infinity, then set to 1.0
+    if (f_scale_factor == -INFINITY)
+      f_scale_factor = 1.0;
+    f_scale_factor = abs(f_scale_factor);
+    printf("\nf_scale_factor: %f", f_scale_factor);
+
+    double f_null_max_limit = 1000.0;
+
+    for (size_t i = 0; i < NUM_DRV; i++)
+    {
+      if (abs(robot.mobile_base->state->pivot_velocities[i]) < pivot_vel_zero_threshold &&
+          count > 1)
+      {
+        f_null[2 * i + 1] *= f_scale_factor;
+
+        if (f_null[2 * i + 1] > f_null_max_limit)
+          f_null[2 * i + 1] = f_null_max_limit;
+        else if (f_null[2 * i + 1] < -f_null_max_limit)
+          f_null[2 * i + 1] = -f_null_max_limit;
+      }
+    }
+    printf("\nf_null scaled:\n");
+    print_matrix(NUM_DRV_COORD, NUM_DRV, f_null);
+
+    double f_drv[NUM_DRV * NUM_DRV_COORD];  // [N]
+    hddc2b_pltf_frc_redu_ref_fini(NUM_DRV, f_krnl, f_null, f_drv);
+    printf("\nf_drv:\n");
+    print_matrix(NUM_DRV_COORD, NUM_DRV, f_drv);
+
+    hddc2b_drv_frc_pvt_to_gnd(NUM_DRV, wheel_distance, castor_offset, f_drv, f_wheel);
+    printf("\nf_wheel:\n");
+    print_matrix(NUM_GND_COORD, NUM_DRV, f_wheel);
+
+    hddc2b_whl_frc_gnd_to_hub(NUM_DRV, wheel_diameter, f_wheel, tau_wheel);
+    printf("\ntau_wheel:\n");
+    print_matrix(NUM_WHL_COORD, NUM_DRV, tau_wheel);
+
+    // saturate torques
+    double tau_limit = 10.0;
+    for (size_t i = 0; i < 8; i++)
+    {
+      if (tau_wheel[i] > tau_limit)
+      {
+        tau_wheel[i] = tau_limit;
+      }
+      else if (tau_wheel[i] < -tau_limit)
+      {
+        tau_wheel[i] = -tau_limit;
+      }
+    }
+
+    double wtau[8];
+    for (size_t i = 0; i < 4; i++)
+    {
+      wtau[2 * i] = tau_wheel[2 * i];
+      wtau[2 * i + 1] = tau_wheel[2 * i + 1];
+    }
+
+    // printf("\nwheel taus:  ");
+    // print_array(wtau, 8);
+
+    printf("\n");
+
+    if (count > 2)
+    {
+      // raise(SIGINT);
+      set_mobile_base_torques(&robot, wtau);
+      update_base_state(robot.mobile_base->mediator->kelo_base_config,
+                        robot.mobile_base->mediator->ethercat_config);
+    }
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto elapsed_time = std::chrono::duration<double>(end_time - start_time);
+
+    // if the elapsed time is less than the desired period, busy wait
+    while (elapsed_time < desired_period)
+    {
+      end_time = std::chrono::high_resolution_clock::now();
+      elapsed_time = std::chrono::duration<double>(end_time - start_time);
+    }
+    control_loop_timestep = elapsed_time.count();
+    // std::cout << "control loop timestep: " << control_loop_timestep << std::endl;
+
+    count++;
+  }
+
+  free_robot_data(&robot);
+
+  return 0;
+}
---
+++ gen/freddy_base_control_right.cpp
@@ -0,0 +1,393 @@
+#include <array>
+#include <string>
+#include <filesystem>
+#include <iostream>
+#include <chrono>
+#include <controllers/pid_controller.hpp>
+#include <motion_spec_utils/utils.hpp>
+#include <motion_spec_utils/math_utils.hpp>
+#include <motion_spec_utils/solver_utils.hpp>
+#include <csignal>
+
+#include <unsupported/Eigen/MatrixFunctions>
+
+#include "motion_spec_utils/log_structs.hpp"
+
+volatile sig_atomic_t flag = 0;
+
+void handle_signal(int sig)
+{
+  static int signal_caught = 0;
+  if (!signal_caught)
+  {
+    signal_caught = 1;
+    flag = 1;
+    printf("Caught signal %d (%s)\n", sig, strsignal(sig));
+  }
+}
+
+// void ramp(double current_vel, double max_vel,
+
+int main(int argc, char **argv)
+{
+  // handle signals
+  struct sigaction sa;
+  sa.sa_handler = handle_signal;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+
+  for (int i = 1; i < NSIG; ++i)
+  {
+    if (sigaction(i, &sa, NULL) == -1)
+    {
+      perror("sigaction");
+    }
+  }
+
+  // read the platform force from the command line
+  double pf[3] = {0.0, 0.0, 0.0};
+  if (argc == 4)
+  {
+    pf[0] = std::stod(argv[1]);
+    pf[1] = std::stod(argv[2]);
+    pf[2] = std::stod(argv[3]);
+  }
+  else
+  {
+    printf(
+        "Usage: ./freddy_base_align <platform_force_x> <platform_force_y> <platform_torque_z>\n");
+    exit(1);
+  }
+
+  // Initialize the robot structs
+  KeloBaseConfig *kelo_base_config = new KeloBaseConfig();
+  kelo_base_config->nWheels = 4;
+  int index_to_EtherCAT[4] = {3, 4, 6, 7};
+  kelo_base_config->index_to_EtherCAT = index_to_EtherCAT;
+  kelo_base_config->radius = 0.115 / 2;
+  kelo_base_config->castor_offset = 0.01;
+  kelo_base_config->half_wheel_distance = 0.0775 / 2;
+  double wheel_coordinates[8] = {0.195, 0.21, -0.195, 0.21, -0.195, -0.21, 0.195, -0.21};
+  kelo_base_config->wheel_coordinates = wheel_coordinates;
+  double pivot_angles_deviation[4] = {0.0, 0.0, 0.0, 0.0};
+  kelo_base_config->pivot_angles_deviation = pivot_angles_deviation;
+
+  EthercatConfig *ethercat_config = new EthercatConfig();
+
+  MobileBase<Robile> freddy_base;
+  Robile robile;
+  robile.ethercat_config = ethercat_config;
+  robile.kelo_base_config = kelo_base_config;
+
+  freddy_base.mediator = &robile;
+  freddy_base.state = new MobileBaseState();
+
+  Freddy robot = {nullptr, nullptr, &freddy_base};
+
+  // get current file path
+  std::filesystem::path path = __FILE__;
+
+  // get the robot urdf path
+  std::string robot_urdf =
+      (path.parent_path().parent_path() / "urdf" / "freddy_corrected_base.urdf").string();
+
+  char ethernet_interface[100] = "eno1";
+  initialize_robot(&robot, robot_urdf, ethernet_interface);
+
+  const double desired_frequency = 1000.0;                                             // Hz
+  const auto desired_period = std::chrono::duration<double>(1.0 / desired_frequency);  // s
+  double control_loop_timestep = desired_period.count();                               // s
+  double *control_loop_dt = &control_loop_timestep;                                    // s
+
+  // pid controller variables
+  double Kp = 1.5;
+  double Ki = 0.1;
+  double Kd = 0.0;
+
+  double w_dist_prev_error[4] = {0.0, 0.0, 0.0, 0.0};
+  double w_dist_error_sum[4] = {0.0, 0.0, 0.0, 0.0};
+
+  double pf_lin_x_vel_prev_error = 0.0;
+  double pf_lin_x_vel_error_sum = 0.0;
+  double pf_lin_y_vel_prev_error = 0.0;
+  double pf_lin_y_vel_error_sum = 0.0;
+  double pf_ang_z_vel_prev_error = 0.0;
+  double pf_ang_z_vel_error_sum = 0.0;
+
+  double plat_vel_setpoint[3] = {0.05, 0.05, 0.25};
+  double plat_clip_force = 20.0;
+  double plat_sat_force = 300.0;
+
+  double pivot_vel_zero_threshold = 4.5;
+  double pivot_vel_ramp_factor = 0.1;
+  double ff_tau_c = 50.0;
+  double current_ff_tau_c[4] = {0.0, 0.0, 0.0, 0.0};
+  double plat_vel_damping_tube = 0.001;
+  double plat_vel_damping_factor = 0.1;
+  double plat_vel_attenuation_factor = 0.2;
+  double pivot_align_error_margin = 0.25;
+
+  double plat_vel_xy_pid_controller_kp = 200.0;
+  double plat_vel_xy_pid_controller_ki = 0.0;
+  double plat_vel_xy_pid_controller_kd = 0.0;
+
+  double plat_vel_ang_pid_controller_kp = 100.0;
+  double plat_vel_ang_pid_controller_ki = 0.0;
+  double plat_vel_ang_pid_controller_kd = 0.0;
+
+  double plat_vel_x_prev_error = 0.0;
+  double plat_vel_y_prev_error = 0.0;
+  double plat_vel_ang_prev_error = 0.0;
+
+  double plat_vel_x_error_sum = 0.0;
+  double plat_vel_y_error_sum = 0.0;
+  double plat_vel_ang_error_sum = 0.0;
+
+  update_base_state(robot.mobile_base->mediator->kelo_base_config,
+                    robot.mobile_base->mediator->ethercat_config);
+  get_robot_data(&robot, *control_loop_dt);
+
+  int count = 0;
+
+  while (true)
+  {
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    if (flag)
+    {
+      printf("Exiting somewhat cleanly...\n");
+      free_robot_data(&robot);
+      exit(0);
+    }
+
+    printf("count: %d\n", count);
+
+    get_robot_data(&robot, *control_loop_dt);
+
+    // printf("odometry: ");
+    // print_array(robot.mobile_base->state->x_platform, 3);
+
+    double plat_force[3] = {pf[0], pf[1], pf[2]};  // [N], [N], [Nm]
+
+    // damping controller to regulate the platform velocity
+    double plat_vel_damping_force[3] = {0.0, 0.0, 0.0};
+    double plat_vel_error[3] = {0.0, 0.0, 0.0};
+
+    // check if platform velocity is greater than the setpoint
+    for (int i = 0; i < 3; ++i)
+    {
+      if (plat_force[i] > plat_clip_force &&
+          robot.mobile_base->state->xd_platform[i] > plat_vel_setpoint[i])
+        computeEqualityError(robot.mobile_base->state->xd_platform[i], plat_vel_setpoint[i],
+                             plat_vel_error[i]);
+      else if (plat_force[i] < -plat_clip_force &&
+               robot.mobile_base->state->xd_platform[i] < -plat_vel_setpoint[i])
+        computeEqualityError(robot.mobile_base->state->xd_platform[i], -plat_vel_setpoint[i],
+                             plat_vel_error[i]);
+      else
+        plat_vel_error[i] = 0.0;
+    }
+
+    // PID controller for regulating the platform velocity
+    pidController(plat_vel_error[0], plat_vel_xy_pid_controller_kp, plat_vel_xy_pid_controller_ki,
+                  plat_vel_xy_pid_controller_kd, *control_loop_dt, plat_vel_x_error_sum, 5.0,
+                  plat_vel_x_prev_error, plat_vel_damping_force[0]);
+    pidController(plat_vel_error[1], plat_vel_xy_pid_controller_kp, plat_vel_xy_pid_controller_ki,
+                  plat_vel_xy_pid_controller_kd, *control_loop_dt, plat_vel_y_error_sum, 5.0,
+                  plat_vel_y_prev_error, plat_vel_damping_force[1]);
+    pidController(plat_vel_error[2], plat_vel_ang_pid_controller_kp,
+                  plat_vel_ang_pid_controller_ki, plat_vel_ang_pid_controller_kd, *control_loop_dt,
+                  plat_vel_ang_error_sum, 5.0, plat_vel_ang_prev_error, plat_vel_damping_force[2]);
+
+    // adjust the platform force based on the damping controller
+    // for (size_t i = 0; i < 3; i++)
+    // {
+    //   if (fabs(plat_vel_error[i]) > plat_vel_damping_tube)
+    //   {
+    //     plat_force[i] += plat_vel_damping_force[i];
+
+    //     // Dynamically reduce force to prevent overshooting
+    //     if ((plat_force[i] > 0.0 && plat_vel_error[i] < 0.0) ||
+    //         (plat_force[i] < 0.0 && plat_vel_error[i] > 0.0))
+    //     {
+    //       plat_force[i] *= 1.0 - fabs(plat_vel_error[i]) * plat_vel_damping_factor;
+    //     }
+    //   }
+    //   else if (plat_force[i] < plat_clip_force && plat_force[i] > -plat_clip_force)
+    //   {
+    //     // Smooth clipping adjustment
+    //     plat_force[i] *= (1.0 - plat_vel_attenuation_factor);
+    //   }
+    // }
+
+    // saturation
+    for (size_t i = 0; i < 3; i++)
+    {
+      if (plat_force[i] > plat_sat_force)
+      {
+        plat_force[i] = plat_sat_force;
+      }
+      else if (plat_force[i] < -plat_sat_force)
+      {
+        plat_force[i] = -plat_sat_force;
+      }
+    }
+
+    printf("platform force: ");
+    print_array(plat_force, 3);
+
+    // printf("platform velocity: ");
+    // print_array(robot.mobile_base->state->xd_platform, 3);
+
+    printf("pivot angles:     ");
+    print_array(robot.mobile_base->state->pivot_angles, 4);
+
+    printf("pivot velocities: ");
+    print_array(robot.mobile_base->state->pivot_velocities, 4);
+
+    double lin_offsets[4];
+    double ang_offsets[4];
+    get_pivot_alignment_offsets(&robot, plat_force, lin_offsets, ang_offsets);
+
+    // transform the platform force by 90 degrees ccw
+    Eigen::Rotation2Dd pf_correction_rot(M_PI / 2);
+    Eigen::Vector2d lin_pf = Eigen::Vector2d(plat_force[0], plat_force[1]);
+
+    // compute the weights for the platform force
+    double platform_weights[2];
+    platform_weights[0] =
+        abs(plat_force[2]) < 1e-6
+            ? 1.0
+            : sqrt(pow(plat_force[0], 2) + pow(plat_force[1], 2)) /
+                  (sqrt(pow(plat_force[0], 2) + pow(plat_force[1], 2) + pow(plat_force[2], 2)));
+
+    platform_weights[1] = 1.0 - platform_weights[0];
+
+    double lin_force_weight = lin_pf.norm() == 0.0 ? 0.0 : platform_weights[0];
+    double moment_weight = plat_force[2] == 0.0 ? 0.0 : platform_weights[1];
+
+    double tau_align[4]{};
+    double dist_align[4]{};
+
+    for (size_t i = 0; i < robot.mobile_base->mediator->kelo_base_config->nWheels; i++)
+    {
+      dist_align[i] = lin_offsets[i] * lin_force_weight + ang_offsets[i] * moment_weight;
+    }
+
+    double dist_align_errors[4]{};
+
+    for (size_t i = 0; i < robot.mobile_base->mediator->kelo_base_config->nWheels; i++)
+    {
+      computeEqualityError(dist_align[i], 0.0, dist_align_errors[i]);
+      if (abs(dist_align_errors[i]) > pivot_align_error_margin)
+        pidController(dist_align_errors[i], Kp, Ki, Kd, control_loop_timestep, w_dist_error_sum[i],
+                      20.0, w_dist_prev_error[i], tau_align[i]);
+    }
+
+    printf("dist align errors: ");
+    print_array(dist_align_errors, 4);
+
+    // ramp the ff torques based on the pivot velocities
+    double tau_align_c[8]{};
+    for (size_t i = 0; i < 4; i++)
+    {
+      tau_align_c[2 * i] = tau_align[i];
+      tau_align_c[2 * i + 1] = tau_align[i];
+    }
+
+    printf("alignmnt torques: ");
+    print_array(tau_align_c, 8);
+
+    for (size_t i = 0; i < 4; i++)
+    {
+      if (abs(robot.mobile_base->state->pivot_velocities[i]) < pivot_vel_zero_threshold &&
+          abs(tau_align[i]) > 0.0 && count > 2)
+      {
+        printf("adding ff tau to pivot %d\n", i);
+        current_ff_tau_c[i] = ff_tau_c;
+
+        // Ramp the ff_tau_c value up
+        // if (current_ff_tau_c[i] < ff_tau_c)
+        // {
+        //   current_ff_tau_c[i] += pivot_vel_ramp_factor;
+        //   if (current_ff_tau_c[i] > ff_tau_c)
+        //     current_ff_tau_c[i] = ff_tau_c;
+        // }
+      }
+      else
+      {
+        current_ff_tau_c[i] = 0.0;
+        // Ramp the ff_tau_c value down
+        // if (current_ff_tau_c[i] > 0.0)
+        // {
+        //   current_ff_tau_c[i] -= pivot_vel_ramp_factor;
+        //   if (current_ff_tau_c[i] < 0.0)
+        //     current_ff_tau_c[i] = 0.0;
+        // }
+      }
+
+      tau_align_c[2 * i] +=
+          dist_align_errors[i] < 0.0 ? -current_ff_tau_c[i] : current_ff_tau_c[i];
+      tau_align_c[2 * i + 1] +=
+          dist_align_errors[i] < 0.0 ? -current_ff_tau_c[i] : current_ff_tau_c[i];
+    }
+
+    printf("align taus w ff:  ");
+    print_array(tau_align_c, 8);
+
+    double tau_wheel_c[8]{};
+    base_fd_solver_cgls(&robot, plat_force, tau_align_c, tau_wheel_c);
+
+    // saturate torques
+    double tau_limit = 10.0;
+    for (size_t i = 0; i < 8; i++)
+    {
+      if (tau_wheel_c[i] > tau_limit)
+      {
+        tau_wheel_c[i] = tau_limit;
+      }
+      else if (tau_wheel_c[i] < -tau_limit)
+      {
+        tau_wheel_c[i] = -tau_limit;
+      }
+    }
+
+    double wtau[8]{};
+    for (size_t i = 0; i < 4; i++)
+    {
+      wtau[2 * i] = tau_wheel_c[2 * i];
+      wtau[2 * i + 1] = tau_wheel_c[2 * i + 1];
+    }
+
+    printf("wheel taus:       ");
+    print_array(wtau, 8);
+
+    printf("\n");
+
+    if (count > 2)
+    {
+      // raise(SIGINT);
+      set_mobile_base_torques(&robot, wtau);
+      update_base_state(robot.mobile_base->mediator->kelo_base_config,
+                        robot.mobile_base->mediator->ethercat_config);
+    }
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto elapsed_time = std::chrono::duration<double>(end_time - start_time);
+
+    // if the elapsed time is less than the desired period, busy wait
+    while (elapsed_time < desired_period)
+    {
+      end_time = std::chrono::high_resolution_clock::now();
+      elapsed_time = std::chrono::duration<double>(end_time - start_time);
+    }
+    control_loop_timestep = elapsed_time.count();
+    // std::cout << "control loop timestep: " << control_loop_timestep << std::endl;
+
+    count++;
+  }
+
+  free_robot_data(&robot);
+
+  return 0;
+}
